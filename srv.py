1273a10b7a01882f8ecfef4ed0cb011e9eaf9557
Reuben Waring <reuben.butt6@gmail.com>
2020-08-01 20:58:52 +0100

    Upload v1

---
+++ srv.py
@@ -0,0 +1,21 @@
+# Python3.7+
+import socket
+
+HOST, PORT = '', 8888
+f = open("main.txt")
+page = f.read()
+f.close()
+text = bytes(page, 'utf-8')
+listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+listen_socket.bind((HOST, PORT))
+listen_socket.listen(1)
+print("Serving on Port "+str(PORT))
+while True:
+    client_connection, client_address = listen_socket.accept()
+    request_data = client_connection.recv(1024)
+    print(request_data.decode('utf-8'))
+
+    http_response = text
+    client_connection.sendall(http_response)
+    client_connection.close()
